58b836e5238f21b5a1b4e5e8ef701185027f1731
Chelsea-exe <cjackson145@student.gsu.edu>
2019-10-28 23:34:35 -0400

    update v2

--- liri.js
+++ liri.js
@@ -1,5 +1,62 @@
+//spotify keys from .env file
 require("dotenv").config();
 
+//accessing the API Keys, axios
 var keys = require("./keys.js");
+var fs = require("fs");
+var axios = require("axios");
+var spotify = require('node-spotify-api');
+var Spotify = new spotify(keys.spotify);
+var moment = require('moment');
 
-var spotify = new spotify(keys.spotify);
+
+var request;
+var response;
+
+//search for concerts via BandsInTown
+function concertThis() {
+    var queryUrl = "https://rest.bandsintown.com/artists/" + request + "/events?app_id=codingbootcamp"
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//access command line argv to acquire the file we need const [node, file, arg] = process.argv;
+
+//
+//if (process.argv[0] === "movie-this") {
+//    newMovie("The Lion King");
+//}
+//else {
+//    newMovie(argv.)
+//}
